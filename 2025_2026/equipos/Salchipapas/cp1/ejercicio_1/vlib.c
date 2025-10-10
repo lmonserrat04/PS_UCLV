a0563f3fa94178f83d3f5cf0dadafbeaf4a8b2b1
amaliezzzz <amirods05@gmail.com>
2025-10-10 17:53:59 -0400

    feat: add v_norm and v_mean functions

--- 2025_2026/equipos/Salchipapas/cp1/ejercicio_1/vlib.c
+++ 2025_2026/equipos/Salchipapas/cp1/ejercicio_1/vlib.c
@@ -65,6 +65,33 @@ double v_get(const Vector *v, size_t i) {
     return v->data[i];
 }
 
+double v_norm(const Vector *v) {
+    if (!v || !v->data) {
+        fprintf(stderr, "Error: Vector no inicializado.\n");
+        return NAN;
+    }
+    
+    double sum_squares = 0.0;
+    for (size_t i = 0; i < v->size; i++) {
+        sum_squares += v->data[i] * v->data[i];
+    }
+    return sqrt(sum_squares);
+}
+
+
+double v_mean(const Vector *v) {
+    if (!v || !v->data || v->size == 0) {
+        fprintf(stderr, "Error: Vector no inicializado.\n")
+        return NAN;
+    }
+    
+    double sum = 0.0;
+    for (size_t i = 0; i < v->size; i++) {
+        sum += v->data[i];
+    }
+    return sum / v->size;
+}
+
 Vector* v_add(const Vector *a, const Vector *b) {
     if (!a || !b || !a->data || !b->data || a->size != b->size) {
         fprintf(stderr, "Error: Vectores inválidos o de diferente tamaño.\n");
